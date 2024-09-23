c818c762fff31c29ce3b18718e031963497cfb92
minhaj-mhd <minhajmuhamd@gmail.com>
2024-09-23 17:28:14 +0530

    added design for home and products page

--- ecom/products/urls.py
+++ ecom/products/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("index",views.index, name = "index")
+    path("index",views.index, name = "index"),
+    path("products/",views.ListProducts, name = "products")
 ]
--- ecom/products/views.py
+++ ecom/products/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render (request,"index.html")
+def ListProducts(request):
+    """g"""
+    return render(request,"list_layout.html")
