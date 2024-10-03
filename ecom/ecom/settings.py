82faf601ceeae5761690e0f8f98e94d6b8487d1f
minhaj-mhd <minhajmuhamd@gmail.com>
2024-10-03 22:14:36 +0530

    added celery and redis

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -128,8 +128,23 @@ STATICFILES_DIRS = [
 MEDIA_URL = 'images/'
 MEDIA_ROOT = os.path.join( BASE_DIR, "media/")
 
+CELERY_BROKER_URL = 'redis://localhost:6379/0'  # URL for Redis as the broker
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://172.30.239.178:6379/1",  # Replace with your WSL IP
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
